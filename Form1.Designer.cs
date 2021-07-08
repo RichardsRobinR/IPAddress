88212e6bc38e2f2422c507a903ca22c2d1278aa7
RichardsRobinR <richardsrobin.r15@gmail.com>
2021-07-08 19:48:49 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace IPAddress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipadress_Name = new System.Windows.Forms.Label();
+            this.ipaddress_Textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ipadress_Name
+            // 
+            this.ipadress_Name.AutoSize = true;
+            this.ipadress_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipadress_Name.Location = new System.Drawing.Point(40, 41);
+            this.ipadress_Name.Name = "ipadress_Name";
+            this.ipadress_Name.Size = new System.Drawing.Size(299, 108);
+            this.ipadress_Name.TabIndex = 0;
+            this.ipadress_Name.Text = "label1";
+            // 
+            // ipaddress_Textbox
+            // 
+            this.ipaddress_Textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipaddress_Textbox.Location = new System.Drawing.Point(45, 200);
+            this.ipaddress_Textbox.Name = "ipaddress_Textbox";
+            this.ipaddress_Textbox.Size = new System.Drawing.Size(718, 116);
+            this.ipaddress_Textbox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ipaddress_Textbox);
+            this.Controls.Add(this.ipadress_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ipadress_Name;
+        private System.Windows.Forms.TextBox ipaddress_Textbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPAddress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getIpaddress();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ipadress_Name.Text = "IP Address";
+            this.ipaddress_Textbox.Text = getIpaddress();
+        }
+
+        String getIpaddress()
+        {
+            Process process = new Process();
+            
+            process.StartInfo.FileName = "ipconfig.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            StreamReader reader = process.StandardOutput;
+            string output = reader.ReadToEnd();
+
+            //Console.WriteLine(output);
+
+            process.WaitForExit();
+
+            var getIpadress = output.IndexOf("IPv4 Address. . . . . . . . . . . :");
+
+
+            return output.Substring((getIpadress + 36), 17);
+        }
+    }
+}
